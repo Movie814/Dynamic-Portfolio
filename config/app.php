8305fb0673c008d525ab442d90feec2bed050f25
coder <moviedownload814@gmail.com>
2022-05-08 12:26:30 -0500

    asset fixed

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL'),
+    'secure_asset_url' => env('secure_asset_URL'),
 
     /*
     |--------------------------------------------------------------------------
