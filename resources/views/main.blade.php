70dccbdb45d8ea29595dbaf3504d31f497df986c
coder <moviedownload814@gmail.com>
2022-05-09 03:40:23 -0500

    commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,11 +26,11 @@
             </div>
             <div class="form-group col-md-3 mt-4"> 
                <div> <label for="title"> <h6> <b>Title</b> </h6> </label>
-                <input type="text" class="form-control" name="title" id="title" value="{{(@$main->title)?$main->title:"The Title"}}" >
+                <input type="text" class="form-control" name="title" id="title" value="{{$main->title}}" >
             </div>
                <div>
                 <label for="sub_title"><h6> <b>Sub Title</b> </h6></label>
-                <input type="text" class="form-control" name="sub_title" id="sub_title" value="{{(@$main->sub_title)?$main->sub_title:"Sub Title"}}" >
+                <input type="text" class="form-control" name="sub_title" id="sub_title" value="{{$main->sub_title}}" >
                </div>
                <div>
                 <label for="sub_title"><h6> <b>Upload Resume</b> </h6></label>
@@ -39,7 +39,7 @@
             </div>
             
         </div>
-        <input type="submit" class="btn btn-primary mt-4" name="" id="">
+        <input type="submit" class="btn btn-primary mt-4" name="" id="" value="Send Data">
     </div>
 </form>
 </main>
