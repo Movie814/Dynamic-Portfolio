35724765e55e6dd6f3e06953acf6b1e43fbe8d64
coder <moviedownload814@gmail.com>
2022-05-12 12:50:14 -0500

    make it better

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,11 +26,11 @@
             </div>
             <div class="form-group col-md-3 mt-4"> 
                <div> <label for="title"> <h6> <b>Title</b> </h6> </label>
-                <input type="text" class="form-control" name="title" id="title" value="{{$main->title}}" >
+                <input type="text" class="form-control" name="title" id="title" value="{{(@$main->title)?$main->title:"This Is Title"}}" >
             </div>
                <div>
                 <label for="sub_title"><h6> <b>Sub Title</b> </h6></label>
-                <input type="text" class="form-control" name="sub_title" id="sub_title" value="{{$main->sub_title}}" >
+                <input type="text" class="form-control" name="sub_title" id="sub_title" value="{{(@$main->sub_title)?$main->sub_title:"This Is Sub Title"}}" >
                </div>
                <div>
                 <label for="sub_title"><h6> <b>Upload Resume</b> </h6></label>
