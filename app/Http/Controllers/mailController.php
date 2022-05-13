c34ad684eb98501627b74e8614766b5fe3d3b13e
coder <moviedownload814@gmail.com>
2022-05-13 01:12:35 -0500

    final

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -10,15 +10,15 @@ class mailController extends Controller
     public function sendmail(Request $request){
        
        
-    
         $data=['name'=>$request->name,'email'=>$request->email,'phone'=>$request->phone,'msg'=>$request->msg];
-        // $user['to']='moviedownload814@gmail.com';
-        Mail::send('mail',$data,function ($mes) use ($data){
-            $mes-> to($data['email']);
+        $user['to']='moviedownload814@gmail.com';
+        Mail::send('mail',$data,function ($mes) use ($user ,$data){
+            $mes-> to($user['to']);
             $mes->subject($data['name']);
             $mes->from('moviedownload814@gmail.com');
-            return redirect()->back();
+            
         });
+        return redirect()->back();
         
     }
 }
